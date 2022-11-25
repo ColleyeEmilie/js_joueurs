34e0305d7478dd3e3153d2f8886f3bbbf12be9b8
emilie <colleyeemilie@hotmail.com>
2022-11-25 15:18:05 +0100

    update

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ class Player{
         return this.score = score+1;
     }
 }
-
-const button1 = document.querySelector('.button1');
-const button2 = document.querySelector('.button2');
+const buttons = document.querySelectorAll('button');
 const joueur1 = new Player('Luana', 0);
 const joueur2 = new Player('Jordan', 0);
 
-button1.addEventListener('click', ()=>{
-    joueur1.newScore(joueur1.score);
-    button1.textContent = `Le score de ${joueur1.nom} est de ${joueur1.score}`;
-})
-
-button2.addEventListener('click', ()=>{
-    joueur2.newScore(joueur2.score);
-    button2.textContent = `Le score de ${joueur2.nom} est de ${joueur2.score}`;
-})
+for (const button of buttons) {
+    button.addEventListener('click', (event)=>{
+        if(event.currentTarget.getElementById === 'button1') {
+            joueur1.newScore(joueur1.score);
+            event.currentTarget.textContent = `Le score de ${joueur1.nom} est de ${joueur1.score}`;
+        } else {
+            joueur2.newScore(joueur2.score);
+            event.currentTarget.textContent = `Le score de ${joueur2.nom} est de ${joueur2.score}`;
+        }
+    })
+}
